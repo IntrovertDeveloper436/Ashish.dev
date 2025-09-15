84a20b6b8da54e0102e02b9f36b7d75708cb817a
Ashish448 <ashishkumarjena1437@gmail.com>
2025-09-15 10:08:35 +0530

    base structure added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+
+  // You can add more options later if needed
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
